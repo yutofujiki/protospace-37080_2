48126cdd099d51f38ba7e98a0ad340d0a9201dcd
yutofujiki <y0u5t0o4.fujippy@gmail.com>
2022-01-06 14:42:52 +0900

    投稿したプロトタイプがトップページで表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'prototypes/index'
   root to: "prototypes#index"
-  resources :prototypes, only: [:create, :new]
+  resources :prototypes, only: [:create, :new, :index]
 end
