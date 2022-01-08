3bc55ed7522bcdd988a18c3efb0d7e2780764e05
yutofujiki <y0u5t0o4.fujippy@gmail.com>
2022-01-08 16:54:01 +0900

    投稿者以外のユーザーが、投稿者専用のページに遷移できないよう

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,8 @@
 class PrototypesController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_edit, only: :edit
+
   def index
     @prototypes = Prototype.all
   end
@@ -46,6 +50,13 @@ class PrototypesController < ApplicationController
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_edit
+    @prototype = Prototype.find(params[:id])
+    unless @prototype.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 
 end
